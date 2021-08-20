0b7ffec6dbdcb0dc44d1c602312b96d8527aab34
imdiode <nandanparagshukla@gmail.com>
2021-08-20 18:42:06 +0530

    hamburger menu

--- Js/header.js
+++ Js/header.js
@@ -1,8 +1,20 @@
 $(document).on('scroll', function() {
     var wi = 1200;
-    //if (document.getElementById("navlist").clientWidth > 1200) {
-    wi = document.getElementById("navlist").clientWidth - 300;
-    //}
-    $('h1').css("left", Math.max(wi - 30 - 2 * window.scrollY, 5) + "px");
-    $('h3').css("left", Math.max(wi - 2 * window.scrollY, 30) + "px");
+    const windowWidth = document.getElementById("navlist").clientWidth;
+    wi = windowWidth - 300;
+    if (windowWidth > 620) {
+        $('h1').css("left", Math.max(wi - 30 - 2 * window.scrollY, 5) + "px");
+        $('h3').css("left", Math.max(wi - 2 * window.scrollY, 30) + "px");
+    } else {
+        $('h1').css("left", ((windowWidth - 150) / 2) + "px");
+        $('h3').css("left", ((windowWidth - 120) / 2) + "px");
+    }
 })
+
+function menuo() {
+    document.getElementById("resp-menu").style.display = "block";
+}
+
+function menuc() {
+    document.getElementById("resp-menu").style.display = "none";
+}
