fe8f00ec6b7ccd30bda531096366090b5ebb55b7
imdiode <nandanparagshukla@gmail.com>
2021-08-20 09:03:27 +0530

    minor optimizations for desktop site

--- Js/header.js
+++ Js/header.js
@@ -1,4 +1,8 @@
-$(document).on('scroll',function(){
-    $('h1').css("left", Math.max(1200 - 2*window.scrollY,5) + "px");
-    $('h3').css("left", Math.max(1230 - 2*window.scrollY,30) + "px");
-  })
+$(document).on('scroll', function() {
+    var wi = 1200;
+    //if (document.getElementById("navlist").clientWidth > 1200) {
+    wi = document.getElementById("navlist").clientWidth - 300;
+    //}
+    $('h1').css("left", Math.max(wi - 30 - 2 * window.scrollY, 5) + "px");
+    $('h3').css("left", Math.max(wi - 2 * window.scrollY, 30) + "px");
+})
